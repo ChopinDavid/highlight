49b0c3d7cac569797bdcfb67a2188b76ea385238
DavidC <David.Chopin@wwt.com>
2020-09-08 17:31:09 -0500

    WIP shell

--- lib/languages/shell.dart
+++ lib/languages/shell.dart
@@ -1,13 +1,12 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 import '../src/mode.dart';
-import '../src/common_modes.dart';
 
 final shell = Mode(refs: {}, aliases: [
   "console"
 ], contains: [
   Mode(
-      className: "meta",
+      className: "shell-meta",
       begin: "^\\s{0,3}[/\\w\\d\\[\\]()@-]*[>%\$#]",
       starts: Mode(end: "\$", subLanguage: ["bash"]))
 ]);
