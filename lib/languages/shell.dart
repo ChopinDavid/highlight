9a5f5c320d0453fbb852bd08eae56c2e493db374
DavidC <David.Chopin@wwt.com>
2020-09-08 17:40:43 -0500

    WIP shell

--- lib/languages/shell.dart
+++ lib/languages/shell.dart
@@ -6,12 +6,12 @@ final shell = Mode(refs: {}, aliases: [
   "console"
 ], contains: [
   Mode(
-      className: "shell-meta",
-      begin: "^\\s{0,3}[/\\w\\d\\[\\]()@-]*[>%\$#]",
-      starts: Mode(end: "\$", subLanguage: ["bash"])),
-  Mode(
     className: "shell-comment",
     begin: "#",
     end: "\\n",
-  )
+  ),
+  Mode(
+      className: "shell-meta",
+      begin: "^\\s{0,3}[/\\w\\d\\[\\]()@-]*[>%\$#]",
+      starts: Mode(end: "\$", subLanguage: ["bash"])),
 ]);
