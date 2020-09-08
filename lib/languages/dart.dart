445045c819fcd3fcae99d391b4e3552541fdc0e1
DavidC <David.Chopin@wwt.com>
2020-09-08 16:50:23 -0500

    WIP dart

--- lib/languages/dart.dart
+++ lib/languages/dart.dart
@@ -1,17 +1,17 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-import '../src/mode.dart';
 import '../src/common_modes.dart';
+import '../src/mode.dart';
 
 final dart = Mode(refs: {
   '~contains~0~variants~4~contains~2': Mode(
-      className: "subst",
+      className: "dart-subst",
       variants: [Mode(begin: "\\\${", end: "}")],
       keywords: "true false null this is new super",
       contains: [C_NUMBER_MODE, Mode(ref: '~contains~0')]),
-  '~contains~0~variants~4~contains~1':
-      Mode(className: "subst", variants: [Mode(begin: "\\\$[A-Za-z0-9_]+")]),
-  '~contains~0': Mode(className: "string", variants: [
+  '~contains~0~variants~4~contains~1': Mode(
+      className: "dart-subst", variants: [Mode(begin: "\\\$[A-Za-z0-9_]+")]),
+  '~contains~0': Mode(className: "dart-string", variants: [
     Mode(begin: "r'''", end: "'''"),
     Mode(begin: "r\"\"\"", end: "\"\"\""),
     Mode(begin: "r'", end: "'", illegal: "\\n"),
@@ -44,27 +44,27 @@ final dart = Mode(refs: {
       "Comparable DateTime Duration Function Iterable Iterator List Map Match Null Object Pattern RegExp Set Stopwatch String StringBuffer StringSink Symbol Type Uri bool double dynamic int num print Element ElementList document querySelector querySelectorAll window"
 }, contains: [
   Mode(ref: '~contains~0'),
-  Mode(className: "comment", begin: "/\\*\\*", end: "\\*/", contains: [
+  Mode(className: "dart-comment", begin: "/\\*\\*", end: "\\*/", contains: [
     PHRASAL_WORDS_MODE,
     Mode(
-        className: "doctag",
+        className: "dart-doctag",
         begin: "(?:TODO|FIXME|NOTE|BUG|XXX):",
         relevance: 0)
   ], subLanguage: [
     "markdown"
   ]),
-  Mode(className: "comment", begin: "///+\\s*", end: "\$", contains: [
+  Mode(className: "dart-comment", begin: "///+\\s*", end: "\$", contains: [
     Mode(subLanguage: ["markdown"], begin: ".", end: "\$"),
     PHRASAL_WORDS_MODE,
     Mode(
-        className: "doctag",
+        className: "dart-doctag",
         begin: "(?:TODO|FIXME|NOTE|BUG|XXX):",
         relevance: 0)
   ]),
   C_LINE_COMMENT_MODE,
   C_BLOCK_COMMENT_MODE,
   Mode(
-      className: "class",
+      className: "dart-class",
       beginKeywords: "class interface",
       end: "{",
       excludeEnd: true,
@@ -73,6 +73,6 @@ final dart = Mode(refs: {
         UNDERSCORE_TITLE_MODE
       ]),
   C_NUMBER_MODE,
-  Mode(className: "meta", begin: "@[A-Za-z]+"),
+  Mode(className: "dart-meta", begin: "@[A-Za-z]+"),
   Mode(begin: "=>")
 ]);
